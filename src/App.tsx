0c8cf9565dfc518a5d7620c94f37c4848236ac23
Marioslide339 <marioslide.animation@gmail.com>
2026-06-18 14:51:46 +0700

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -44,7 +44,7 @@ import AiChatView from './components/AiChatView';
 import SettingsView from './components/SettingsView';
 import ExpensesView from './components/ExpensesView';
 
-const GOOGLE_SHEET_URL = 'https://script.google.com/macros/s/AKfycbxgblXuPIODMKiUSLtvD6-7QyLdCAGacj9wdkovJ3ERLTUqOZdzVjXmKBZKCXRKQYXWPg/exec';
+const GOOGLE_SHEET_URL = 'https://script.google.com/macros/s/AKfycbzZkzlk43H1GmJTqA3b0KqYr8huYsoZ6R25Tgwe9kguuemdL_6-h8qvPSHUQMc-1n7IaA/exec';
 
 export default function App() {
   const [activeTab, setActiveTab] = useState('dashboard');
