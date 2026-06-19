759bf5503ae281cd5a2b90537dd45c20d207de58
Marioslide339 <marioslide.animation@gmail.com>
2026-06-19 10:15:12 +0700

    Update GoalsViewComponent.tsx

--- src/components/GoalsViewComponent.tsx
+++ src/components/GoalsViewComponent.tsx
@@ -3,7 +3,7 @@
  * SPDX-License-Identifier: Apache-2.0
  */
 
-import { useState, useMemo, useCallback } from 'react';
+import React, { useState, useMemo, useCallback } from 'react';
 import {
   Target,
   TrendingUp,
