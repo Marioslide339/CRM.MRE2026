f5ec977af8a7cdb021b25e0ef40aed09a979c197
Marioslide339 <marioslide.animation@gmail.com>
2026-07-02 12:23:12 +0700

    Update OrdersView.tsx

--- src/components/OrdersView.tsx
+++ src/components/OrdersView.tsx
@@ -23,8 +23,8 @@ const removeAccents = (str: any): string => {
   return s
     .normalize('NFD')
     .replace(/[\u0300-\u036f]/g, '')
-    .replace(/đ/g, 'd')
-    .replace(/Đ/g, 'D');
+    .replace(/\u0111/g, 'd')
+    .replace(/\u0110/g, 'D');
 };
 
 interface OrdersViewProps {
